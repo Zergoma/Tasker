2a10e5ada5254c7e559944afaf9650d28bfd6b46
Galon <Galon.Siff@gmail.com>
2025-12-04 09:29:12 +0100

    Add project files.

---
+++ Tasker/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Tasker.MVVM.Views;
+
+namespace Tasker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainView());
+        }
+    }
+}
---
+++ Tasker/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasker.MVVM.ViewModels
+{
+    public class MainViewModel
+    {
+
+    }
+}
